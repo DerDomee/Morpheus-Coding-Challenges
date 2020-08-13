8252232ed91c7a794c4a54edadce2ae83bda7298
Dominik Riedig <me@dominikriedig.de>
2020-08-13 20:11:36 +0200

    Fix pycodestyle errors

--- solve.py
+++ solve.py
@@ -33,7 +33,7 @@ def print_help():
     # This String looks very proprietary. It is just formatted in a way that
     # It doesnt exceed the 80 char limit in terminal output as well as in code.
     # Sorry for this mess, but it is very clean in terminal output.
-    helpstr = "solve.py " + SCRIPT_VERSION +" - Python interface for the " \
+    helpstr = "solve.py " + SCRIPT_VERSION + " - Python interface for the " \
               "Morpheus Coding Challenges\n\n" \
               "Usage: solve.py <challenge> [runs] [flags...]\t" \
               "Runs the solvescript\n       solve.py --help\t\t\t\tPrint " \
@@ -66,18 +66,21 @@ def is_integer(n):
         float(n)
     except ValueError:
         return False
-    else: return float(n).is_integer()
+    else:
+        return float(n).is_integer()
+
 
 def prompt_yes_no(question, default=None):
     """ Helper function. Prompts the user a yes/no question and returns
         True or false depending on the input
     """
-    valid = {"y": True,
-            "yes": True,
-            "ye": True,
-            "n": False,
-            "no": False
-    }
+    valid = {
+        "y": True,
+        "yes": True,
+        "ye": True,
+        "n": False,
+        "no": False}
+
     if default == "yes":
         prompt = "[Y/n]"
     elif default == "no":
@@ -95,24 +98,22 @@ def prompt_yes_no(question, default=None):
         elif choice in valid:
             return valid[choice]
         else:
-            sys.stderr.write("Please respond with 'Y' or 'N' only. " +
-                             "(Or with yes/no)\n")
-
-
+            sys.stderr.write(
+                "Please respond with 'Y' or 'N' only. "
+                + "(Or with yes/no)\n")
 
 
 def parse_arguments():
     """ Parses given arguments from a stack and returns a
         dictionary with given settings
     """
-    run_args= {
+    run_args = {
         'raw_arguments': sys.argv[1:],
         'parallel_mode': False,
         'run_interactive': False,
         'interactive_mode': False,
         'benchmark_mode': False,
-        'save_raw_results': False
-    }
+        'save_raw_results': False}
 
     challenge_defined = False
     runs_defined = False
@@ -121,24 +122,28 @@ def parse_arguments():
     arg_errored = False
 
     def print_unknown_argument():
-        sys.stderr.write("Error parsing argument '" + argument_instance +
-                         "' at index " + str(i + 1) + ": Unknown argument.\n")
+        sys.stderr.write(
+            "Error parsing argument '" + argument_instance
+            + "' at index " + str(i + 1) + ": Unknown argument.\n")
         arg_errored = True
 
     def print_double_argument():
-        sys.stderr.write("Warning parsing argument '" + argument_instance +
-                         "' at index " + str(i + 1) +
-                         ": Double argument. (Proceeding...)\n")
+        sys.stderr.write(
+            "Warning parsing argument '" + argument_instance
+            + "' at index " + str(i + 1)
+            + ": Double argument. (Proceeding...)\n")
 
     def print_illegal_argument():
-        sys.stderr.write("Error parsing argument '" + argument_instance +
-                         "' at index " + str(i + 1) +
-                         ": Illegal argument or syntax.\n")
+        sys.stderr.write(
+            "Error parsing argument '" + argument_instance
+            + "' at index " + str(i + 1)
+            + ": Illegal argument or syntax.\n")
         arg_errored = True
 
     def print_illegal_help_argument():
-        sys.stderr.write("Error parsing argument '--help' at index " +
-                         str(i + 1) + ": This must be the first argument.\n")
+        sys.stderr.write(
+            "Error parsing argument '--help' at index "
+            + str(i + 1) + ": This must be the first argument.\n")
         arg_errored = True
 
     for i in range(len(arg_stack)):
@@ -152,7 +157,7 @@ def parse_arguments():
             runs_defined = True
             continue
         elif is_integer(argument_instance) and runs_defined and \
-             challenge_defined:
+                challenge_defined:
             print_unknown_argument()
             arg_errored = True
             continue
@@ -208,14 +213,14 @@ def check_arguments(run_args):
         sys.stderr.write("Error checking arguments: " + custom_message + "\n")
 
     def print_warning_state(custom_message="Unknown warning"):
-        sys.stderr.write("Warning checking arguments: " + custom_message +
-                         " (Proceeding...)\n")
+        sys.stderr.write("Warning checking arguments: " + custom_message
+                         + " (Proceeding...)\n")
 
-    if not "challenge_number" in run_args:
+    if "challenge_number" not in run_args:
         print_illegal_state("No challenge number defined.")
         checking_errored = True
 
-    if not "runs_number" in run_args:
+    if "runs_number" not in run_args:
         run_args['runs_number'] = 1
 
     if "challenge_number" in run_args and run_args['challenge_number'] < 1:
@@ -227,22 +232,20 @@ def check_arguments(run_args):
         checking_errored = True
 
     if "runs_number" in run_args and run_args['runs_number'] == 1 and \
-      run_args['benchmark_mode']:
-        print_warning_state("Benchmarking only makes sense with more than 1 " +
-                            "run. Turning off benchmark mode.")
+            run_args['benchmark_mode']:
+        print_warning_state("Benchmarking only makes sense with more than 1 "
+                            + "run. Turning off benchmark mode.")
         run_args['benchmark_mode'] = False
 
     if "runs_number" in run_args and run_args['runs_number'] > 100 and \
-      run_args['parallel_mode']:
-        print_warning_state("Parallel mode limits the maximum amount " +
-                            "of runs to 100 to prevent rate limit crashing. " +
-                            "Applying limit.")
+            run_args['parallel_mode']:
+        print_warning_state("Parallel mode limits the maximum amount "
+                            + "of runs to 100 to prevent rate limit crashing. "
+                            + "Applying limit.")
         run_args['runs_number'] = 100
 
     if checking_errored:
         sys.exit(102)
-
-
 
 
 def interactive_pass(run_args):
@@ -252,7 +255,7 @@ def interactive_pass(run_args):
     if not run_args['interactive_mode']:
         return
 
-    setstr =  "--------------------"
+    setstr = "--------------------"
     setstr += "\nChallenge to run: {}".format(run_args['challenge_number'])
     setstr += "\nNumber of runs: {}".format(run_args['runs_number'])
     setstr += "\nParallel Mode: {}".format(run_args['parallel_mode'])
@@ -270,23 +273,20 @@ def solve_instance(run_args, run_data, timing_data, i, solvefile):
     run_data['runs_awaiting'] -= 1
 
     timing_instance = {
-        'total_start': time.time()
-    }
+        'total_start': time.time()}
 
     challenge = requests.get(
-        "https://cc.the-morpheus.de/challenges/" +
-        str(run_args['challenge_number']) + "/"
-    )
+        "https://cc.the-morpheus.de/challenges/"
+        + str(run_args['challenge_number']) + "/")
 
     timing_instance['runtime_start'] = time.time()
     solution_instance = solvefile.solve(challenge.text)
     timing_instance['runtime_end'] = time.time()
 
     solution_result = requests.post(
-        "https://cc.the-morpheus.de/solutions/" +
-        str(run_args['challenge_number']) + "/",
-        solution_instance
-    )
+        "https://cc.the-morpheus.de/solutions/"
+        + str(run_args['challenge_number']) + "/",
+        solution_instance)
 
     if "Error" in solution_result.text:
         run_data['wrong_solutions'] += 1
@@ -300,7 +300,7 @@ def solve_instance(run_args, run_data, timing_data, i, solvefile):
     t_key = "c" + str(run_args['challenge_number']) + "r" + str(i)
     timing_data[t_key] = timing_instance
 
-    run_data['runs_finished'] +=1
+    run_data['runs_finished'] += 1
     run_data['runs_in_progress'] -= 1
 
     return
@@ -315,27 +315,27 @@ def solve_challenges(run_args):
         'runs_in_progress': 0,
         'runs_finished': 0,
         'wrong_solutions': 0,
-        'ctf_token': None
-    }
+        'ctf_token': None}
+
     timing_data = {
-        'total_start_time': time.time()
-    }
+        'total_start_time': time.time()}
+
     solvefile = None
+
     try:
-        solvefile = importlib.import_module('challenges.chall' +
-                                            str(run_args['challenge_number']))
+        solvefile = importlib.import_module(
+            'challenges.chall' + str(run_args['challenge_number']))
     except ModuleNotFoundError:
-        sys.stderr.write("Error while starting solving of challenges: " +
-                         "This Challenge has no corresponding " +
-                         "solution file.\n")
+        sys.stderr.write("Error while starting solving of challenges: "
+                         + "This Challenge has no corresponding "
+                         + "solution file.\n")
         sys.exit(105)
 
     for i in range(run_args['runs_number']):
         if(run_args['parallel_mode']):
             _thread.start_new_thread(
                 solve_instance,
-                (run_args, run_data, timing_data, i, solvefile)
-            )
+                (run_args, run_data, timing_data, i, solvefile))
         else:
             sys.stdout.write("\rRunning Loop " + str(i + 1))
             solve_instance(run_args, run_data, timing_data, i, solvefile)
@@ -347,8 +347,8 @@ def solve_challenges(run_args):
 
     result_data = {
         'run_data': run_data,
-        'timing_data': timing_data
-    }
+        'timing_data': timing_data}
+
     sys.stdout.write("\n")
     return result_data
 
@@ -368,42 +368,43 @@ def parse_result(run_args, run_result):
         'best_inst_totaldelta': 1000000,
         'worst_inst_totaldelta': -1,
         'best_inst_rundelta': 1000000,
-        'worst_inst_rundelta': -1
-    }
+        'worst_inst_rundelta': -1}
 
     for i in range(run_args['runs_number']):
         key = "c" + str(run_args['challenge_number']) + "r" + str(i)
         timing_data[key]['total_delta'] = timing_data[key]['total_end'] - \
-                                          timing_data[key]['total_start']
+            timing_data[key]['total_start']
         if timing_data[key]['total_delta'] > \
-           statistics['worst_inst_totaldelta']:
-           statistics['worst_inst_totaldelta'] = timing_data[key]['total_delta']
+                statistics['worst_inst_totaldelta']:
+            statistics['worst_inst_totaldelta'] = \
+                timing_data[key]['total_delta']
 
         if timing_data[key]['total_delta'] < \
-            statistics['best_inst_totaldelta']:
-            statistics['best_inst_totaldelta'] = timing_data[key]['total_delta']
+                statistics['best_inst_totaldelta']:
+            statistics['best_inst_totaldelta'] = \
+                timing_data[key]['total_delta']
 
         timing_data[key]['run_delta'] = timing_data[key]['runtime_end'] - \
-                                       timing_data[key]['runtime_start']
+            timing_data[key]['runtime_start']
 
         if timing_data[key]['run_delta'] > \
-           statistics['worst_inst_rundelta']:
-           statistics['worst_inst_rundelta'] = timing_data[key]['run_delta']
+                statistics['worst_inst_rundelta']:
+            statistics['worst_inst_rundelta'] = timing_data[key]['run_delta']
 
         if timing_data[key]['run_delta'] < \
-           statistics['best_inst_rundelta']:
-           statistics['best_inst_rundelta'] = timing_data[key]['run_delta']
+                statistics['best_inst_rundelta']:
+            statistics['best_inst_rundelta'] = timing_data[key]['run_delta']
 
         statistics['runtime_delta'] += timing_data[key]['run_delta']
         statistics['total_delta'] += timing_data[key]['total_delta']
 
     statistics['total_quotdelta'] = statistics['total_delta'] / \
-                                    run_args['runs_number']
+        run_args['runs_number']
     statistics['runtime_quotdelta'] = statistics['runtime_delta'] / \
-                                      run_args['runs_number']
-
+        run_args['runs_number']
 
     return statistics
+
 
 def print_result(run_args, run_result, statistics):
     """ Print results of the solving process.
@@ -411,63 +412,51 @@ def print_result(run_args, run_result, statistics):
     """
     print("\n------------------------------------------------------------")
     print(
-        "Ergebnisse für Challenge " +
-        str(run_args['challenge_number']) + "\n"
-    )
+        "Ergebnisse für Challenge "
+        + str(run_args['challenge_number']) + "\n")
     print(
-        "Anzahl der Versuche: " +
-        str(run_args['runs_number'])
-    )
+        "Anzahl der Versuche: "
+        + str(run_args['runs_number']))
     print(
-        "Anzahl der Fehlversuche: " +
-        str(run_result['run_data']['wrong_solutions'])
-    )
+        "Anzahl der Fehlversuche: "
+        + str(run_result['run_data']['wrong_solutions']))
     print(
-        "CTF-Token: " +
-        str(run_result['run_data']['ctf_token'])
-    )
+        "CTF-Token: "
+        + str(run_result['run_data']['ctf_token']))
     if(run_args['benchmark_mode']):
         print()
         print(
-            "                      Benchmark-Laufzeit: " +
-            str(round(statistics['total_delta'] * 1000, 6)) +
-            "ms"
-        )
+            "                      Benchmark-Laufzeit: "
+            + str(round(statistics['total_delta'] * 1000, 6))
+            + "ms")
         print(
-            "                        Lösungs-Laufzeit: " +
-            str(round(statistics['runtime_delta'] * 1000, 6)) +
-            "ms\n"
-        )
+            "                        Lösungs-Laufzeit: "
+            + str(round(statistics['runtime_delta'] * 1000, 6))
+            + "ms\n")
         print(
-            "         Kürzeste totale Instanzlaufzeit: " +
-            str(round(statistics['best_inst_totaldelta'] * 1000, 6)) +
-            "ms"
-        )
+            "         Kürzeste totale Instanzlaufzeit: "
+            + str(round(statistics['best_inst_totaldelta'] * 1000, 6))
+            + "ms")
         print(
-            "          Längste totale Instanzlaufzeit: " +
-            str(round(statistics['worst_inst_totaldelta'] * 1000, 6)) +
-            "ms"
-        )
+            "          Längste totale Instanzlaufzeit: "
+            + str(round(statistics['worst_inst_totaldelta'] * 1000, 6))
+            + "ms")
         print(
-            "Durchschnittliche totale Instanzlaufzeit: " +
-            str(round(statistics['total_quotdelta'] * 1000, 6)) +
-            "ms\n"
-        )
+            "Durchschnittliche totale Instanzlaufzeit: "
+            + str(round(statistics['total_quotdelta'] * 1000, 6))
+            + "ms\n")
         print(
-            "                Kürzeste Instanzlaufzeit: " +
-            str(round(statistics['best_inst_rundelta'] * 1000, 6)) +
-            "ms"
-        )
+            "                Kürzeste Instanzlaufzeit: "
+            + str(round(statistics['best_inst_rundelta'] * 1000, 6))
+            + "ms")
         print(
-            "                 Längste Instanzlaufzeit: " +
-            str(round(statistics['worst_inst_rundelta'] * 1000, 6)) +
-            "ms"
-        )
+            "                 Längste Instanzlaufzeit: "
+            + str(round(statistics['worst_inst_rundelta'] * 1000, 6))
+            + "ms")
         print(
-            "       Durchschnittliche Instanzlaufzeit: " +
-            str(round(statistics['runtime_quotdelta'] * 1000, 6)) +
-            "ms"
-        )
+            "       Durchschnittliche Instanzlaufzeit: "
+            + str(round(statistics['runtime_quotdelta'] * 1000, 6))
+            + "ms")
     print("------------------------------------------------------------")
 
 
@@ -481,6 +470,7 @@ def main():
     statistics = parse_result(run_args, run_result)
     print_result(run_args, run_result, statistics)
     return 0
+
 
 if __name__ == "__main__":
     exit(main())
